601020ae0959dc1f27131a0e2e2aba114b7aa5af
James Talton <James.Talton@ibm.com>
2017-10-30 17:36:53 -0400

    Make unit test more robust

--- test/routes/database-collection.spec.js
+++ test/routes/database-collection.spec.js
@@ -1,6 +1,11 @@
+require("should")
+
 describe("GET /", function () {
     it("should return 200 on success", function () {
         return this.request.get("/")
-            .expect(200, ["things"])
+            .expect(200)
+            .then((response) => {
+                response.body.should.containDeep(["things"])
+            })
     })
 })
